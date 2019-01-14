b3fea02f701ebf126d1bdc664672f1f5078dfe7b
Jaco <jaco.bornman@sybrin.co.za>
2019-01-14 15:34:28 +0200

    Initial Commit
    Initial commit to github repo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# RecruitementTest
-Test for recruits
+## 1. Welcome  
+Welcome to the DevOps recruitment test. In this test you are given an incomplete project.    
+To ensure we can see the progress you make please paste the given identifier in the following section:  
+  
+Your goals are as follows:  
+## 2. Solution Breakdown
+* Result Application = Node.js    
+* Vote Application = Python    
+* Worker Application = .NET    
+* Cache Management = Redis    
+* Storage = MySQL    
+  
+## 2.1. Setup the solution    
+* The solution required will be:    
+    * A web-based voting application with two options (Intel vs. AMD)    
+    * Every vote is processed by the application    
+    * Cached to Redis    
+    * Saved to MySQL    
+    * Button to generate a report on all votes the system received    
+* Steps to follow:    
+    * Get all the parts running locally  
+    * Move all components to containers  
+    * Enable cross container communication  
+    * Test the solution  
+* Containerize the solution (links added to get you going)  
+    * Containerizing .NET: [Link](https://docs.docker.com/engine/examples/dotnetcore/#prerequisites)  
+    * Containerizing Jenkins: [Link](https://github.com/jenkinsci/docker/blob/master/README.md)  
+    * Containerizing Redis: [Link](https://hub.docker.com/_/redis)  
+    * Containerizing Node: [Link](https://nodejs.org/en/docs/guides/nodejs-docker-webapp/)  
+* Automate the deployment of the solution  
+    * Jenkins should be used for managing your containers and building the solution  
+   
+## 3. Notes
+
+* You may choose how to containerize the solution  
+* Redis connection string will like this:   
+    ```localhost, connectTimeout=5000, syncTimeout=5000```  
+* Use container-based Jenkins  
+* Make On  
+  
+  
---
+++ worker/dotnet/Worker/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Worker.Data;
+using Worker.Entities;
+using Worker.Messaging;
+using Worker.Workers;
+
+namespace Worker
+{
+    class Program
+    {
+        private static string address = "http://auzpoc.sybrin.co.za/<your_guid_here>";
+        public class RootObject
+        {
+            public string hostname { get; set; }
+            public DateTime time { get; set; }
+            public string user { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            RootObject data = new RootObject();
+
+            string hostname = Environment.MachineName;
+            DateTime time = DateTime.Now;
+            string user = Environment.UserName;
+            data.hostname = hostname;
+            data.time = time;
+            data.user = user;
+
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(data));
+            using (var client = new System.Net.WebClient())
+            {
+                client.UploadData(address, "PUT", bytes);
+            }
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var loggerFactory = new LoggerFactory()
+                .AddConsole();
+
+            var services = new ServiceCollection()
+                .AddSingleton(loggerFactory)
+                .AddLogging()
+                .AddSingleton<IConfiguration>(config)
+                .AddTransient<IVoteData, MySqlVoteData>()
+                .AddTransient<IMessageQueue, MessageQueue>()
+                .AddSingleton<QueueWorker>()
+                .AddDbContext<VoteContext>(builder => builder.UseMySQL(config.GetConnectionString("VoteData")));
+
+            var provider = services.BuildServiceProvider();
+            var worker = provider.GetService<QueueWorker>();
+            worker.Start();
+        }
+    }
+}
